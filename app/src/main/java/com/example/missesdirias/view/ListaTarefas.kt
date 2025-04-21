0e173e3edde725e2e91d00f48391adbeba6200a2
Gustavo M. de Farias <gumf2004@outlook.com>
2025-04-21 15:52:22 -0300

    Adicionado arquivos do projeto.

---
+++ app/src/main/java/com/example/missesdirias/view/ListaTarefas.kt
@@ -0,0 +1,12 @@
+package com.example.missesdirias.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ListaTarefas(
+    navController: NavController
+){
+    Text(text = "OI")
+}
---
+++ app/src/main/java/com/example/missesdirias/view/SalvarTarefa.kt
@@ -0,0 +1,11 @@
+package com.example.missesdirias.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SalvarTarefa(
+    navController: NavController
+){
+
+}
